daf9089977895840e6dedb9489bc543c80a154eb
Chris Birdsong <34635830+smtpman@users.noreply.github.com>
2023-04-06 22:32:32 -0500

    Update Omada-Install-java11-v5.9.31.sh

--- Omada-Install-java11-v5.9.31.sh
+++ Omada-Install-java11-v5.9.31.sh
@@ -3,6 +3,9 @@
 # It is recommended to update repository and upgrade system before installing dependencies and omada software controller:
 sudo apt update && sudo apt upgrade -y
 
+# Install omada dependencies available over the repo (Java8, jsvc and curl)
+sudo apt install -y openjdk-8-jre-headless jsvc curl
+
 # Install libssl1.1 which is dependency for MongoDB 4.4
 wget http://security.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2.17_amd64.deb
 sudo apt install -y ./libssl1.1_1.1.1f-1ubuntu2.17_amd64.deb
@@ -13,8 +16,6 @@ wget -qO- https://www.mongodb.org/static/pgp/server-4.4.asc | sudo tee /etc/apt/
 sudo add-apt-repository -y "deb [arch=amd64] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse"
 sudo apt install -y mongodb-org
 
-# Install omada dependencies available over the repo (Java8, jsvc and curl)
-sudo apt install -y openjdk-11-jre-headless jsvc curl
 
 # Download and install Omada Controller Software
 wget https://static.tp-link.com/upload/software/2023/202303/20230321/Omada_SDN_Controller_v5.9.31_Linux_x64.deb
